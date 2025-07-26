4b68281a96218d81a6b5b4805b011ccd74eb6fa2
Abhayj2004 <abhaybj2004@gmail.com>
2025-07-26 20:06:12 +0530

    added print

--- src/add.kt
+++ src/add.kt
@@ -1,3 +1,16 @@
-class add {
-
+fun main(){
+    val name ="Aparna"
+    val age=20
+    println("name:$name")
+    println("Age:$age")
+    println("Welcome to kotlin Assignment!")
+    if (age >= 18)
+    {
+        println("you are an adult")
+    }
+    else
+    {
+        println("you are not an adult")
+    }
+    println("Program finished")
 }
